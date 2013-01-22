c858103edaaac7e98d103d9f549b89c0c86a499c
zettlunic <marcel@luniclabs.at>
2013-01-22 16:24:06 +0100

    initial commit

---
+++ jquery.hider.js
@@ -0,0 +1,28 @@
+(function( $ ) {
+  $.fn.hider = function(options) {
+  
+    // Set the default options
+	myoptions = jQuery.extend({
+		wrapper: "wrapper",
+		header: "header",
+		content: "content",
+		time: "200"
+	}, options);
+
+	//create class strings
+	var wrapper = "." + myoptions.wrapper;
+	var header = "." + myoptions.header;
+	var content = "." + myoptions.content;
+
+	//hide and show handling
+	$(header).click(function() {
+	var content_div = $(this).next(content);
+
+	if (content_div.css('display') == 'none') {
+		  	content_div.slideDown(200);
+	} else {
+		  	content_div.slideUp(200);
+		}
+	});
+  };
+})( jQuery );
